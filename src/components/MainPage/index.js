42604b92b164cf0a6af3d65bb43b016a86475d93
irina-dzikun <irina.dzikun@gmail.com>
2023-10-09 13:00:09 +0200

    Add Welcome

--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 
+import styles from "./style.module.css";
+
 import Header from "../Header";
 
 const MainPage = () => {
   return (
     <div>
-      <Header></Header>
+      <div className={styles.container_main}>
+        <Header></Header>
+        <div className={styles.welcome_box}>
+          <div className={styles.welcome_text}>
+            Welcome<br></br> to the Brooklyn Library
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
