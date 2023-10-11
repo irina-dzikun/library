77f060de6e332e3bd3e0c42a6fdee47fae117d88
irina-dzikun <irina.dzikun@gmail.com>
2023-10-11 11:38:54 +0200

    Implement pick season logic

--- src/components/FavoriteBook/index.js
+++ src/components/FavoriteBook/index.js
@@ -2,25 +2,19 @@ import React from "react";
 
 import styles from "./style.module.css";
 
-import theBookEaters from "../../images/the-book-eaters.png";
-
-const FavoriteBook = () => {
+const FavoriteBook = ({ book }) => {
   return (
     <div className={styles.container}>
       <div className={styles.container_book}>
         <div className={styles.staff}>Staff Picks</div>
         <div className={styles.line}></div>
-        <div className={styles.name_book}>The Book Eaters</div>
-        <div className={styles.writer}>By Sunyi Dean</div>
-        <div className={styles.description_book}>
-          An unusual sci-fi story about a book eater woman who tries desperately to save her dangerous mind-eater son
-          from tradition and certain death. Complete with dysfunctional family values, light Sapphic romance, and a
-          strong, complex protagonist. Not for the faint of heart.
-        </div>
+        <div className={styles.name_book}>{book.name}</div>
+        <div className={styles.writer}>{book.writer}</div>
+        <div className={styles.description_book}>{book.description}</div>
         <button className={styles.button_buy}>Buy</button>
       </div>
       <div className={styles.image_box}>
-        <img src={theBookEaters} alt="theBookEaters" className={styles.image} />
+        <img src={book.image} alt="theBookEaters" />
       </div>
     </div>
   );
--- src/components/PickSeason/index.js
+++ src/components/PickSeason/index.js
@@ -1,45 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./style.module.css";
 
 import FavoriteBook from "../FavoriteBook";
+import booksData from "../../data.js";
 
 const PickSeason = () => {
+  const allBooks = booksData;
+  const [selectedSeason, setSelectedSeason] = useState("0");
+
+  const booksSeason = allBooks[selectedSeason].map((item) => item);
+
+  function handleChange(e) {
+    const { id } = e.target;
+    setSelectedSeason();
+    setSelectedSeason(id);
+  }
+  console.log(typeof selectedSeason);
+
   return (
     <div>
       <div>
         <form className={styles.favorite_form}>
           <div className={styles.favorite_radio}>
             <label>
-              <input type="radio" value="winter" checked className={styles.favorite_form_dot} />
+              <input
+                type="radio"
+                id="0"
+                value="winter"
+                className={styles.favorite_form_dot}
+                checked={selectedSeason === "0"}
+                onChange={handleChange}
+              />
               Winter
             </label>
           </div>
           <div>
             <label>
-              <input type="radio" value="spring" className={styles.favorite_form_dot} />
+              <input
+                type="radio"
+                id="1"
+                value="spring"
+                className={styles.favorite_form_dot}
+                checked={selectedSeason === "1"}
+                onChange={handleChange}
+              />
               Spring
             </label>
           </div>
           <div>
             <label>
-              <input type="radio" value="summer" className={styles.favorite_form_dot} />
+              <input
+                type="radio"
+                id="2"
+                value="summer"
+                className={styles.favorite_form_dot}
+                checked={selectedSeason === "2"}
+                onChange={handleChange}
+              />
               Summer
             </label>
           </div>
           <div>
             <label>
-              <input type="radio" value="autumn" className={styles.favorite_form_dot} />
+              <input
+                type="radio"
+                id="3"
+                value="autumn"
+                className={styles.favorite_form_dot}
+                checked={selectedSeason === "3"}
+                onChange={handleChange}
+              />
               Autumn
             </label>
           </div>
         </form>
       </div>
       <div className={styles.favorite_books}>
-        <FavoriteBook className={styles.favorite_book}></FavoriteBook>
-        <FavoriteBook className={styles.favorite_book}></FavoriteBook>
-        <FavoriteBook className={styles.favorite_book}></FavoriteBook>
-        <FavoriteBook className={styles.favorite_book}></FavoriteBook>
+        <FavoriteBook className={styles.favorite_book} book={booksSeason[0]}></FavoriteBook>
+        <FavoriteBook className={styles.favorite_book} book={booksSeason[1]}></FavoriteBook>
+        <FavoriteBook className={styles.favorite_book} book={booksSeason[2]}></FavoriteBook>
+        <FavoriteBook className={styles.favorite_book} book={booksSeason[3]}></FavoriteBook>
       </div>
     </div>
   );
