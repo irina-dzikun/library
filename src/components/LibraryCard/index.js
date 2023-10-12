c2f37a56150fcbabb3fe03baeb76214cbd3beed9
irina-dzikun <irina.dzikun@gmail.com>
2023-10-12 14:56:46 +0200

    Add LibraryCard component

---
+++ src/components/LibraryCard/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import styles from "./style.module.css";
+
+const LibraryCard = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <div className={styles.name_card}>Brooklyn Public Library</div>
+        <input type="text" maxlength="20" placeholder="Reader's name" className={styles.input} />
+        <input type="text" maxlength="20" placeholder="Card number" className={styles.input} />
+      </div>
+      <div>
+        <button className={styles.check_button}>Check the card</button>
+      </div>
+    </div>
+  );
+};
+
+export default LibraryCard;
