577bfcadaf0de34bdbe11f65921e272ed7fc6449
irina-dzikun <irina.dzikun@gmail.com>
2023-10-19 15:27:53 +0200

    Data refactoring

--- src/data.js
+++ src/data.js
@@ -30,7 +30,7 @@ const booksData = [
       id: 1,
       season: "winter",
       name: "The Book Eaters",
-      writer: "By Sunyi Dean",
+      writer: "Sunyi Dean",
       description:
         "An unusual sci-fi story about a book eater woman who tries desperately to save her dangerous mind-eater son from tradition and certain death. Complete with dysfunctional family values, light Sapphic romance, and a strong, complex protagonist. Not for the faint of heart.       ",
       image: winterOne,
@@ -39,7 +39,7 @@ const booksData = [
       id: 2,
       season: "winter",
       name: "Cackle",
-      writer: "By Rachel Harrison",
+      writer: "Rachel Harrison",
       description:
         "Are your Halloween movies of choice The Witches of Eastwick and Practical Magic? Look no further than here - where a woman recovering from a breakup moves to a quaint town in upstate New York and befriends a beautiful witch.",
       image: winterTwo,
@@ -48,7 +48,7 @@ const booksData = [
       id: 3,
       season: "winter",
       name: "Dante: Poet of the Secular World",
-      writer: "By Erich Auerbach",
+      writer: "Erich Auerbach",
       description:
         "Auerbach's engaging book places the 'Comedy' within the tradition of epic, tragedy, and philosophy in general, arguing for Dante's uniqueness as one who raised the individual and his drama of soul into something of divine significance—an inspired introduction to Dante's main themes.",
       image: winterThree,
@@ -57,7 +57,7 @@ const booksData = [
       id: 4,
       season: "winter",
       name: "The Last Queen",
-      writer: "By Clive Irving",
+      writer: "Clive Irving",
       description:
         "A timely and revelatory new biography of Queen Elizabeth (and her family) exploring how the Windsors have evolved and thrived as the modern world has changed around them.",
       image: winterFour,
@@ -68,7 +68,7 @@ const booksData = [
       id: 1,
       season: "spring",
       name: "The Body",
-      writer: "By Stephen King",
+      writer: "Stephen King",
       description:
         "Powerful novel that takes you back to a nostalgic time, exploring both the beauty and danger and loss of innocence that is youth.",
       image: springOne,
@@ -77,7 +77,7 @@ const booksData = [
       id: 2,
       season: "spring",
       name: "Carry: A Memoir of Survival on Stolen Land",
-      writer: "By Toni Jenson",
+      writer: "Toni Jenson",
       description:
         "This memoir about the author's relationship with gun violence feels both expansive and intimate, resulting in a lyrical indictment of the way things are.",
       image: springTwo,
@@ -86,7 +86,7 @@ const booksData = [
       id: 3,
       season: "spring",
       name: "Days of Distraction",
-      writer: "By Alexandra Chang",
+      writer: "Alexandra Chang",
       description:
         "A sardonic view of Silicon Valley culture, a meditation on race, and a journal of displacement and belonging, all in one form-defying package of spare prose.",
       image: springThree,
@@ -95,7 +95,7 @@ const booksData = [
       id: 4,
       season: "spring",
       name: "Dominicana",
-      writer: "By Angie Cruz",
+      writer: "Angie Cruz",
       description:
         "A fascinating story of a teenage girl who marries a man twice her age with the promise to bring her to America. Her marriage is an opportunity for her family to eventually immigrate. For fans of Isabel Allende and Julia Alvarez.",
       image: springFour,
@@ -106,7 +106,7 @@ const booksData = [
       id: 1,
       season: "summer",
       name: "Crude: A Memoir",
-      writer: "By Pablo Fajardo & Sophie Tardy-Joubert",
+      writer: "Pablo Fajardo & Sophie Tardy-Joubert",
       description:
         "Drawing and color by Damien Roudeau | This book illustrates the struggles of a group of indigenous Ecuadoreans as they try to sue the ChevronTexaco company for damage their oil fields did to the Amazon and her people",
       image: summerOne,
@@ -115,7 +115,7 @@ const booksData = [
       id: 2,
       season: "summer",
       name: "Let My People Go Surfing",
-      writer: "By Yvon Chouinard",
+      writer: "Yvon Chouinard",
       description:
         "Chouinard—climber, businessman, environmentalist—shares tales of courage and persistence from his experience of founding and leading Patagonia, Inc. Full title: Let My People Go Surfing: The Education of a Reluctant Businessman, Including 10 More Years of Business Unusual.",
       image: summerTwo,
@@ -124,7 +124,7 @@ const booksData = [
       id: 3,
       season: "summer",
       name: "The Octopus Museum: Poems",
-      writer: "By Brenda Shaughnessy",
+      writer: "Brenda Shaughnessy",
       description:
         "This collection of bold and scathingly beautiful feminist poems imagines what comes after our current age of environmental destruction, racism, sexism, and divisive politics.",
       image: summerThree,
@@ -133,7 +133,7 @@ const booksData = [
       id: 4,
       season: "summer",
       name: "Shark Dialogues: A Novel",
-      writer: " By Kiana Davenport",
+      writer: "Kiana Davenport",
       description:
         "An epic saga of seven generations of one family encompasses the tumultuous history of Hawaii as a Hawaiian woman gathers her four granddaughters together in an erotic tale of villains and dreamers, queens and revolutionaries, lepers and healers.",
       image: summerFour,
@@ -144,7 +144,7 @@ const booksData = [
       id: 1,
       season: "autumn",
       name: "Casual Conversation",
-      writer: "By Renia White",
+      writer: "Renia White",
       description:
         "White's impressive debut collection takes readers through and beyond the concepts of conversation and the casual - both what we say to each other and what we don't, examining the possibilities around how we construct and communicate identity. ",
       image: autumnOne,
@@ -153,7 +153,7 @@ const booksData = [
       id: 2,
       season: "autumn",
       name: "The Great Fire",
-      writer: "By Lou Ureneck",
+      writer: "Lou Ureneck",
       description:
         "The harrowing story of an ordinary American and a principled Naval officer who, horrified by the burning of Smyrna, led an extraordinary rescue effort that saved a quarter of a million refugees from the Armenian Genocide",
       image: autumnTwo,
@@ -162,7 +162,7 @@ const booksData = [
       id: 3,
       season: "autumn",
       name: "Rickey: The Life and Legend",
-      writer: "By Howard Bryant",
+      writer: "Howard Bryant",
       description:
         "With the fall rolling around, one can't help but think of baseball's postseason coming up! And what better way to prepare for it than reading the biography of one of the game's all-time greatest performers, the Man of Steal, Rickey Henderson?",
       image: autumnThree,
@@ -171,7 +171,7 @@ const booksData = [
       id: 4,
       season: "autumn",
       name: "Slug: And Other Stories",
-      writer: "By Megan Milks",
+      writer: "Megan Milks",
       description:
         "Exes Tegan and Sara find themselves chained together by hairballs of codependency. A father and child experience the shared trauma of giving birth to gods from their wounds.",
       image: autumnFour,
